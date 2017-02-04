bcca3d9346cfb3d3a641f8c3d1386f6d1536d598
Chenxu Jiang <chj028@ucsd.edu>
2017-02-04 14:47:34 -0800

    Login js 20% done

--- submarine/client/templates/login/login.js
+++ submarine/client/templates/login/login.js
@@ -1,7 +1,53 @@
 Template.Login.onCreated(function() {
 
+  var self = this;  // self is template instance
+
+  // vars attach to the template instance
+  self.usernameKeyPress = -1;     // unix timestamp last keypress
+  self.emailKeyPress = -1;        // unix timestamp last keypress
+
+  self.usernameRegex = App.Services.Users.usernameRegex;
+  self.emailRegex = /.+@(.+){2,}\.(.+){2,}/;
+
+  self.usernameTimeout = -1;
+  self.emailTimeout = -1;
+
+  // convenience function to display error or approve function
+  self.displaySpan = function(type, jDOM, msg) {
+
+    // remove previous span under jDOM
+    jDOM.remove('span');
+
+    var html = "<span class=\"" + type + "\">" + msg? msg : "";
+
+    if (type == "pass")
+      html += "<i class=\"fa fa-check-circle\"></i></span>";
+    else if (type == "error")
+      html += "<i class=\"fa fa-minus-circle\"></i></span>";
+    else if (type == "load")
+      html += "<i class=\"fa fa-refresh fa-spin\"></i></span>";
+
+    jDOM.append(html);
+  };
 });
 
 Template.login.events({
+
+  // switch to sign in
+  "click .button[data-action=switch]": function(e, t) {
+    t.$('.lower, .upper').toggleClass('signup')
+                         .toggleClass('login')
+                         .removeClass('forgetPwd');
+  },
+
+  // sign up
+  "click .button[data-action=signup]": function(e, t) {
+
+  },
+
+  // sign in
+  "click .button[data-action=login]": function(e, t) {
+
+  }
 
 });
