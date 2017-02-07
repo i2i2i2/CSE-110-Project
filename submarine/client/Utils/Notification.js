0a63ecf9bdb83999579fa84a9af5301bc784c425
lrt98802 <rtlin97@gmail.com>
2017-02-06 23:54:35 -0800

    Notification wrapper done

--- submarine/client/Utils/Notification.js
+++ submarine/client/Utils/Notification.js
@@ -1,6 +1,6 @@
 /**
  * Wrapper for Cordova plug-in: schedule a single Notification
- * @param: 
+ * @param:
  *      message: the main message to be displayed as Notification
  * Desired outcome: Displaying a single message
  * by default: "Yellow Submarine" would be displayed as title
@@ -8,17 +8,8 @@
 
 App.Utils.Notification.scheduleSingleNotification = function(message){
     cordova.plugins.notification.local.schedule({
-        id: 1, 
+        id: 1,
         title: "Yellow Submarine",
-        text: message       
+        text: message
     });
-};
-
-
-/** 
- * Wrapper for Cordova plug-in: click event
- */
-App.Utils.Notification.clickNotification = function(message) {
-    App.Utils.Notification.scheduleSingleNotification(message);
-    
 };
