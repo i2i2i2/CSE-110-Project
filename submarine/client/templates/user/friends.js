01edad91bc0468110b01f5f0aa668b71a43d16b9
Zijin <apple@appledeMacBook-Pro-10.local>
2017-02-09 16:45:57 -0800

    add friends frame

--- submarine/client/templates/user/friends.js
+++ submarine/client/templates/user/friends.js
@@ -1,3 +1,25 @@
 Template.Friends.onRendered(function() {
+  var self = this;
+
   Session.set("currentTemplate", "friends");
 });
+
+Template.Friends.helpers({
+  recommendations: function() {
+    // return Meteor.user.profile.recommedations;
+    return [
+      {
+        name: "Hello",
+        description: "World"
+      },
+      {
+        name: "Hello1",
+        description: "World1"
+      },
+      {
+        name: "Hello2",
+        description: "World2"
+      }
+    ]
+  }
+})
