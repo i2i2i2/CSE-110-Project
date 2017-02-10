9bae56b78f43b1ee82efa59e47f64be150ee91b9
ShuYi Ni <ShuYi Ni>
2017-02-10 01:24:19 -0800

    static main page

--- submarine/client/templates/user/home.js
+++ submarine/client/templates/user/home.js
@@ -1,3 +1,30 @@
 Template.Home.onRendered(function() {
   Session.set("currentTemplate", "home");
 });
+
+Template.Home.helpers({
+
+    "greetingName": function() {
+        return "Sheep";
+    },
+
+    "recentTags": function() {
+        return [
+            "tag1",
+            "tag2",
+            "tag3",
+            "tag4",
+            "tag5",
+            ];
+    },
+
+    "recentFriends": function() {
+        return [
+            "friend1",
+            "friend2",
+            "friend3",
+            "friend4",
+            "friend5",
+            ];
+    }
+})
