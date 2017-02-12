05fe11cd7b3d175c5453c7853916267d67835582
Zijin <apple@appledeMacBook-Pro-10.local>
2017-02-11 17:48:55 -0800

    hide navigation bar on friendProfile page

--- submarine/client/templates/user/friendProfile.js
+++ submarine/client/templates/user/friendProfile.js
@@ -1,5 +1,10 @@
-Template.Friends.onRendered(function() {
+Template.FriendProfile.onRendered(function() {
   var self = this;
-
+  $(".bottom.nav").addClass("hidden");
   Session.set("currentTemplate", "friend_profile");
+});
+
+
+Template.FriendProfile.onDestroyed(function() {
+  $(".bottom.nav").removeClass("hidden");
 });
