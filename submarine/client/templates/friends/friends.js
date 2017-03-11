b767e851c1900ec69152ac306f57838406f121e4
Chenxu Jiang <chj028@ucsd.edu>
2017-03-11 11:46:40 -0800

    add recent msg in friend page

--- submarine/client/templates/friends/friends.js
+++ submarine/client/templates/friends/friends.js
@@ -12,11 +12,11 @@ Template.Friends.helpers({
     Meteor.user().profile.friendRequest : null,
 
   "emptyFriends": function() {
-        if(Meteor.user().profile.friends == null || Meteor.user().profile.friends.length === 0) {
-            return true;
-        }
-    },
-  
+    if(Meteor.user().profile.friends == null || Meteor.user().profile.friends.length == 0) {
+      return true;
+    }
+  },
+
   hasRecommendation: function() {
     if(Meteor.user().profile.recommendedFriends.length != 0){
       return true;
@@ -33,14 +33,46 @@ Template.Friends.helpers({
 
   profileSeed: (id) => Meteor.users.findOne(id).profile.profileSeed,
 
-  getName: (id, nickname) => nickname? nickname: Meteor.users.findOne(id).username,
+  getName: (friend) => friend.nickname? friend.nickname: Meteor.users.findOne(friend.userId).username,
 
-  friendList: () => Meteor.userId()?
-  Meteor.user().profile.friends : null,
+  friendChatList: () => {
+    var latestMsg = Session.get("latestMsg");
+    var friends = Meteor.user().profile.friends;
 
+    if (!latestMsg) return friends;
+
+    friends = friends.map((friend) => {
+      if (latestMsg[friend.userId]) {
+        friend.message = latestMsg[friend.userId].message;
+        friend.time = latestMsg[friend.userId].time;
+      }
+      return friend;
+    }).filter(friend => !!friend.time).sort(function(friend1, friend2) {
+      return -friend1.time.getTime() + friend2.time.getTime();
+    });
+
+    return friends;
+  },
+
+  friendAlphaList: () => {
+    var list = Meteor.userId()? Meteor.user().profile.friends : null;
+    if (!list || !list.length) return;
+
+    list.sort(function(friend1, friend2) {
+      var name1 = Meteor.users.findOne(friend1.userId).username;
+      var name2 = Meteor.users.findOne(friend2.userId).username;
+      return name1 > name2;
+    });
+
+    console.log(list);
+    return list;
+  },
+  getTime: (time) => {
+    return moment(time).format("HH:mm");
+  }
 });
 
-Template.mainLayout.events({
+Template.Friends.events({
   "click .connect_profile": function (e, t) {
     var idNumber = t.$(e.currentTarget).data('userid');
     FlowRouter.go('/user/friend_profile/'+idNumber);
@@ -50,7 +82,7 @@ Template.mainLayout.events({
     $('#paragraph_text').val('');
     $(".popAdd").css({"display": "none"});
   },
-    
+
   "click .confirm": function () {
     var selfId = Meteor.userId();
     var message = $('#paragraph_text').val();
@@ -63,7 +95,7 @@ Template.mainLayout.events({
     self.friendId = t.$(e.currentTarget).data('userid');
     $(".popAdd").css({"display": "block"});
   },
-    
+
   "click .ignore": function (e, t) {
     var selfId = Meteor.userId();
     var friendId = t.$(e.currentTarget).data('userid');
@@ -72,12 +104,18 @@ Template.mainLayout.events({
   },
 
   "click .dismiss": function (e, t) {
-      var selfId = Meteor.userId();
-      var friendId = t.$(e.currentTarget).data('userid'); Meteor.call('friends/dismissFriend',selfId,friendId);
+    var selfId = Meteor.userId();
+    var friendId = t.$(e.currentTarget).data('userid');
+    Meteor.call('friends/dismissFriend',selfId,friendId);
   },
 
   "click .accept": function (e, t) {
-      var selfId = Meteor.userId();
-      var friendId = t.$(e.currentTarget).data('userid'); Meteor.call('friends/addFriend',selfId,friendId);
+    var selfId = Meteor.userId();
+    var friendId = t.$(e.currentTarget).data('userid');
+    Meteor.call('friends/addFriend',selfId,friendId);
+  },
+
+  "click .switch": function(e, t) {
+    $(".friendList").toggleClass("alpha");
   }
 });
