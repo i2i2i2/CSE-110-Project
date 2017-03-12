b77387bacec86f5103df007c025afa7bb1f006cd
fash97 <Wanhui@Wanhuide-MacBook-Pro.local>
2017-03-11 16:31:51 -0800

    Click the tag and go to chatroom
    Now user shall be able to tap the tag and then enter the specific
    chatroom

--- submarine/server/init.js
+++ submarine/server/init.js
@@ -3,7 +3,7 @@ Meteor.startup(function () {
 
   process.env.ROOT_URL = 'http://100.83.12.125:3000/';
   process.env.MOBILE_ROOT_URL = 'http://100.83.12.125:3000/';
-  process.env.MOBILE_DDP_URL = 'http://100.83.12.125s:3000/';
+  process.env.MOBILE_DDP_URL = 'http://100.83.12.125:3000/';
 
   // seeding database
   App.Seeder.Settings();
