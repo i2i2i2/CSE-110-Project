e45ddb5e7e2614e88129f282c3674915e5e436db
Chenxu Jiang <chj028@ucsd.edu>
2017-03-15 15:14:38 -0700

    style color

--- submarine/client/templates/tags/tagProfile.js
+++ submarine/client/templates/tags/tagProfile.js
@@ -23,6 +23,8 @@ Template.TagProfile.onCreated(function() {
         var tag = App.Collections.Tags.findOne(self.tagId);
         self.tag.set(tag);
         self.repeat.set(tag.repeat);
+
+        
       });
 
     } else {
